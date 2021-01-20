f4ef373bee23b9d5066701dbbffc11b8a9c3e68f
Kota Kamiyama <k.kamikami1211@gmail.com>
2021-01-20 15:49:47 +0900

    add: 状態管理を学ぶ

---
+++ pages/test4.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div v-for="user in users" :key="user.id" :class="$style.container">
+      <div>名前：{{ user.name }}</div>
+      <div>電話番号：{{ user.phone }}</div>
+      <div>メールアドレス：{{ user.email }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    users() {
+      return this.$store.state.user.users
+    },
+  },
+  async created() {
+    await this.$store.dispatch('user/fetchUsers')
+  },
+}
+</script>
+
+<style lang="scss" module>
+.container {
+  margin: 10px 0;
+  padding: 8px;
+  border: 2px solid #ccc;
+  border-radius: 4px;
+}
+</style>
